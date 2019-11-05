5ae4375c97c5ab747d5fe7e388fa46a92b383c8e
rutttaba <42845851+rutttaba@users.noreply.github.com>
2019-11-05 22:07:03 +0200

    correct files

--- index.js
+++ index.js
@@ -1,61 +1,111 @@
-'use strict';
+const dogUrl = 'https://dog.ceo/api/breeds/image/random';
+const catUrl = 'https://aws.random.cat/meow';
+const catFactsUrl = 'http://catfact.ninja/fact';
+const dogFactsUrl = 'http://dog-api.kinduff.com/api/facts';
 
-const apiKey = "pXRKcn7nOvCrwTephATON18r11UBRniSyXLPIuEf";
-const searchUrl = "https://developer.nps.gov/api/v1/parks";
 
-
-function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    return queryItems.join('&');
+function fetchCat() {
+    return fetch(catUrl).then(r => r.json());
 }
 
-function displayResults(responseJson) {
-    console.log(responseJson);
-    $('#results-list').empty();
+function fetchCatFact() {
+    return fetch(catFactsUrl).then(r => r.json());
+}
+
+
+function fetchCatPics(n) {
+    Promise.all(new Array(n).fill(null).map(fetchCat))
+    .then(
+        results => {
+            for (const r of results) {
+                const catPics = new Array().push(`${r.file}`);
+            }
+        }
+    );
+    return catPics;
+}
+
+function fetchCatFacts(n) {
+    Promise.all(new Array(n).fill(null).map(fetchCatFact))
+        .then(
+            results => {
+                for (const r of results) {
+                    const catFacts = new Array().push(`${r.fact}`);
+                }
+            }
+        );
+    return catFacts;
+}
+
+function displayCatResults(n) {
+    $('#results').empty();
     $('#js-error-message').empty();
-    for (let i = 0; i < responseJson.data.length; i++) {
-       
-        $('#results-list').append(
-            `<li><h3>${responseJson.data[i].fullName}</h3>
-      <p>${responseJson.data[i].description}</p>
-       <a href="${responseJson.data[i].url}">Check out the website</a>
-      </li>`
+    for (let i = 0; i < n; i++) {
+        $('#results').append(
+            `<div><img src="${catPics[i]}" alt="photo of a cat">
+             <p class="js-fact">${catFacts[i]}</p></div>
+            `
         )
     };
     $('#results').removeClass('hidden');
-};
+}
 
-function getNationalParks (query, limit = 10) {
-    const params = {
-        stateCode: query,
-        api_key: apiKey,
-        limit
-    };
-    const queryString = formatQueryParams(params)
-    const url = searchUrl + '?' + queryString;
 
-    console.log(url);
 
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
+
+function fetchDog() {
+    return fetch(dogUrl).then(r => r.json());
+}
+function fetchDogFact() {
+    return fetch(dogFactsUrl).then(r => r.json());
+}
+
+function fetchDogPics(n) {
+    Promise.all(new Array(n).fill(null).map(fetchDog)).then(
+        results => {
+            for (const r of results) {
+                const dogPics = new Array().push(`${r.message}`);
             }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson))
-        .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
+        }
+    );
+    return dogPics;
+}
+
+function fetchDogFacts(n) {
+    Promise.all(new Array(n).fill(null).map(fetchDogFact)).then(
+        results => {
+            for (const r of results) {
+                const dogFacts = new Array().push(`${r.facts}`);
+            }
+        }
+    );
+    return dogFacts;
+}
+
+function displayDogResults(n) {
+    $('#results').empty();
+    $('#js-error-message').empty();
+    for (let i = 0; i < n; i++) {
+        $('#results').append(
+            `<div><img src="${dogPics[i]}" alt="photo of a cat">
+             <p class="js-fact">${dogFacts[i]}</p></div>
+            `
+        )
+    };
+    $('#results').removeClass('hidden');
 }
 
 function watchForm() {
+    
+
     $('form').submit(event => {
         event.preventDefault();
-        const searchTerm = $('#js-search-term').val();
-        const limit = $('#js-max-results').val();
-        getNationalParks(searchTerm, limit);
+        const num = $('#number').val();
+        if (button[value] === "cat") {
+            displayCatResults(num)
+        } else if (button[value] === "dog"){
+            displayDogResults(num)
+        }
     });
 }
 
